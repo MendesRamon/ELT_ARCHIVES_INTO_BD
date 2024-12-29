954db65cb38507c9c7067c5d680b7c084761b295
Ramon Mendes <141190770+MendesRamon@users.noreply.github.com>
2024-12-29 20:26:22 -0300

    upload files

---
+++ SQLQueryBD.sql
@@ -0,0 +1,25 @@
+--	Criando data base
+CREATE DATABASE STG_ESTOQUE
+
+--	Criando tabela de stage de dados
+
+USE DW_ESTOQUE
+CREATE TABLE STG_ESTOQUE (
+
+ID_Produto VARCHAR(30) NOT NULL,
+Nome_Produto VARCHAR(50) NOT NULL,
+Categoria VARCHAR(50),
+Data_Entrada DATE NOT NULL,
+Data_Saida DATE,
+Local_Estoque VARCHAR(20),
+Estoque_Inicial INTEGER,
+Entradas INTEGER,
+Saidas INTEGER,
+Quebras INTEGER,
+Perdas INTEGER,
+Estoque_Final INTEGER,
+Tempo_Vida_Produto INTEGER,
+Data_Periodo DATE,
+Loja VARCHAR(15)
+
+)
